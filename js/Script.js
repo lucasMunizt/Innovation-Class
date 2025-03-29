5732027836a75c3cc5217d2d137b2900570a8a3d
lucas <lucasmunizteles@gmail.com>
2025-03-29 10:51:48 -0300

    continuação do menu

---
+++ js/Script.js
@@ -0,0 +1,13 @@
+ const AbrirMenu = () =>{
+  const dropdown = document.querySelector('.container-dropdown');
+  let nomeCor = document.getElementById('todas-categorias');
+
+nomeCor.style.color = nomeCor.style.color ===" rgb(0, 92, 255)" ? "rgb(0, 0, 0)": "rgb(0, 92, 255)";
+
+  dropdown.classList.toggle("active") 
+}
+
+ const MudarCorMenu = () =>{
+    
+ }
+
